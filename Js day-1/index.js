9986697496c12b813304369f070e84b34b149f83
Lokesh <lokeshsingh7695@gmail.com>
2025-01-20 20:20:27 +0530

    JavaScript Day 1

---
+++ Js day-1/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("This is My Internal Js");
+
+let name = "Mukesh";
+console.log(name);
+
+
+document.write("<h1>Hello World</h1>");
+
+document.write("<h1>This is My Internal Js</h1> <h1>This is Another line</h1>");
