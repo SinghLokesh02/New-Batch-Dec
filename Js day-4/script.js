91f8dcc05692d7d28456b46ff61fbabf86469cb0
Lokesh <lokeshsingh7695@gmail.com>
2025-01-30 11:18:03 +0530

    Types of Log and Pop-up msgs

---
+++ Js day-4/script.js
@@ -0,0 +1,26 @@
+// Normal
+console.log("Hi");
+
+// Warning
+console.warn("Hey This is Some Warning")
+
+// Error
+console.error("Hey This is Some Error")
+
+
+// Pop Ups
+
+// Alert
+// alert("Something")
+
+// Confirm
+// let ans = confirm("Are you sure you want to do it?")
+// console.log(ans);
+
+// Prompt
+let age = parseInt(prompt("Enter Your Age"))
+console.log(typeof age);
+
+console.log(age);
+
+
